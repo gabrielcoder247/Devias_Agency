2be8eb07a1680ff9858ce16691c58e47d80e30d2
gabrielcoder <gabrielcoder247@gmail.com>
2019-09-22 10:51:00 +0100

    admin datas

--- Dev_apps/views.py
+++ Dev_apps/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.models import User
+# from django.contrib import messages
+# from django.views import generic
+from django.contrib.auth.decorators import login_required
+# from django.http  import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
+# from django.core.exceptions import ObjectDoesNotExist
+# from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+from .models import *
+from .forms import *
+
 
 # Create your views here.
